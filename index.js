4f16d8c6e045f9cf4d2b96e3c8bbfb0ad9733f0c
aco <arwenceleste.orsi@tptlive.ee>
2023-01-30 15:16:43 +0200

    Add templating

--- index.js
+++ index.js
@@ -2,12 +2,41 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-
+const nunjucks = require('nunjucks');
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
 app.get('/', (req, res) => {
    //console.log(path.resolve(__dirname, 'index.html'));
-  res.sendFile(path.resolve (__dirname, 'index.html'));
+  res.render('index.njk');
+});
+
+
+app.get('/contact', (req, res) => {
+  //console.log(path.resolve(__dirname, 'index.html'));
+ res.render('contact.njk');
+});
+
+
+app.get('/gallery', (req, res) => {
+  //console.log(path.resolve(__dirname, 'index.html'));
+ res.render('gallery.njk');
+});
+
+
+app.get('/about', (req, res) => {
+  //console.log(path.resolve(__dirname, 'index.html'));
+ res.render('about.njk');
+});
+
+app.get('/values', (req, res) => {
+  //console.log(path.resolve(__dirname, 'index.html'));
+ res.render('values.njk');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+ console.log(`Example app listening on port ${port}`)
 });
+
+
